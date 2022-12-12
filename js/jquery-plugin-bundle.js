d898730d6d91458ed1a0960876b234e25eb27b9c
kiran <kirankumarka43@gmail.com>
2022-12-12 15:29:58 +0530

    font defer

--- js/jquery-plugin-bundle.js
+++ js/jquery-plugin-bundle.js
@@ -81,3 +81,25 @@ window.onload = function () {
 
     }
 
+
+    const loadFont = (url) => {
+        // the 'fetch' equivalent has caching issues
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+
+        xhr.onreadystatechange = () => {
+          if (xhr.readyState == 4 && xhr.status == 200) {
+            let css = xhr.responseText;
+            css = css.replace(/}/g, 'font-display: swap; }');
+      
+            const head = document.getElementsByTagName('head')[0];
+            const style = document.createElement('style');
+            style.appendChild(document.createTextNode(css));
+            head.appendChild(style);
+          }
+        };
+        
+        xhr.send();
+      }
+      
+      loadFont('https://fonts.googleapis.com/css2?family=Prata&family=Lato');
